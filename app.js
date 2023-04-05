83ca720ddd0e32d4dc692ca6a36c556b1c32038d
Animesh <animeshs539@gmail.com>
2023-04-05 16:01:00 +0530

    Router and view engine added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
-
+const authorisationRoutes = require("./routes/authorisationRoutes");
 
 
 
 app = express();
+app.set("view engine","ejs");
+
+app.use(authorisationRoutes);
 
 
 app.listen("3000");
